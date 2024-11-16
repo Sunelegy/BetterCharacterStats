3a18d41a0facf9fd9d7ae5aec59b1435e5604495
huolishen <chengda.rd@gmail.com>
2024-11-16 14:16:09 +0800

    Build

---
+++ Localization.lua
@@ -0,0 +1,145 @@
+BCS = BCS or {}
+
+BCS["L"] = {
+	["^Set: Improves your chance to hit by (%d)%%."] = "^套装：使你击中目标的几率提高(%d)%%。",
+	["^Set: Improves your chance to get a critical strike with spells by (%d)%%."] = "^套装：使你的法术造成致命一击的几率提高(%d)%%。",
+	["^Set: Improves your chance to hit with spells by (%d)%%."] = "^套装：使你的法术击中敌人的几率提高(%d+)%%。",
+	["^Set: Increases damage and healing done by magical spells and effects by up to (%d+)%."] = "^套装：提高所有法术和魔法效果所造成的伤害和治疗效果，最多(%d+)点。",
+	["^Set: Increases healing done by spells and effects by up to (%d+)%."] = "^套装：提高法术所造成的治疗效果，最多(%d+)点。",
+	
+	["Equip: Improves your chance to hit by (%d)%%."] = "装备： 使你击中目标的几率提高(%d)%%。",
+	["Equip: Improves your chance to get a critical strike with spells by (%d)%%."] = "装备： 使你的法术造成致命一击的几率提高(%d)%%。",
+	["Equip: Improves your chance to hit with spells by (%d)%%."] = "装备： 使你的法术击中敌人的几率提高(%d)%%。",
+	
+	["Increases your chance to hit with melee weapons by (%d)%%."] = "使你的近战武器击中目标的几率提高(%d)%%。",
+	["Increases your critical strike chance with ranged weapons by (%d)%%."] = "使你的远程武器造成致命一击的几率提高(%d)%%。",
+	["Increases hit chance by (%d)%% and increases the chance movement impairing effects will be resisted by an additional %d+%%."] = "使你的攻击命中敌人的几率提高(%d)%%，并使你抵抗移动限制效果的几率提高%d+%%。",
+	["Increases your critical strike chance with all attacks by (%d)%%."] = "使你的所有攻击造成致命一击的几率提高(%d)%%。",
+	["Increases spell damage and healing by up to (%d+)%% of your total Spirit."] = "使你的法术的治疗和伤害效果提高，数值相当于你的精神值的(%d+)%%。",
+	["Reduces the chance that the opponent can resist your Frost and Fire spells by (%d)%%."] = "使你的目标抵抗你的火焰和冰霜系法术的几率降低(%d)%%。",
+	["Reduces the chance that the opponent can resist your Arcane spells by (%d+)%%."] = "使你的敌人抵抗你的奥术魔法的几率降低(%d+)%%。",
+	["Reduces your target's chance to resist your Shadow spells by (%d+)%%."] = "使目标抵抗你的暗影法术的机率下降(%d+)%%。",
+	
+	["Equip: Increases damage done by Arcane spells and effects by up to (%d+)."] = "装备： 提高奥术法术和效果所造成的伤害，最多(%d+)点。",
+	["Equip: Increases damage done by Fire spells and effects by up to (%d+)."] = "装备： 提高火焰法术和效果所造成的伤害，最多(%d+)点。",
+	["Equip: Increases damage done by Frost spells and effects by up to (%d+)."] = "装备： 提高冰霜法术和效果所造成的伤害，最多(%d+)点。",
+	["Equip: Increases damage done by Holy spells and effects by up to (%d+)."] = "装备： 提高神圣法术和效果所造成的伤害，最多(%d+)点。",
+	["Equip: Increases damage done by Nature spells and effects by up to (%d+)."] = "装备： 提高自然法术和效果所造成的伤害，最多(%d+)点。",
+	["Equip: Increases damage done by Shadow spells and effects by up to (%d+)."] = "装备： 提高暗影法术和效果所造成的伤害，最多(%d+)点。",
+	["Equip: Increases damage done by Shadow spells and effects by up (%d+)."] = "装备： 使暗影法术所造成的伤害提高最多(%d+)点。",
+
+	["Shadow Damage %+(%d+)"] = "暗影伤害 %+(%d+)",
+	["Spell Damage %+(%d+)"] = "%+(%d+) 法术伤害",
+	["Fire Damage %+(%d+)"] = "火焰伤害 %+(%d+)",
+	["Frost Damage %+(%d+)"] = "冰霜伤害 %+(%d+)",
+	["Healing Spells %+(%d+)"] = "治疗法术 %+(%d+)",
+	["Healing Spells and Damage %+(%d+)"] = "治疗和法术伤害 %+(%d+)",
+	
+	["Equip: Restores (%d+) mana per 5 sec."] = "装备： 每5秒回复(%d+)点法力值。",
+	["+(%d)%% Hit"] = "+(%d)%% 命中",
+	
+	-- Random Bonuses // https://wow.gamepedia.com/index.php?title=SuffixId&oldid=204406
+	["^%+(%d+) Damage and Healing Spells"] = "^%+(%d+) 伤害和治疗法术",
+	["^%+(%d+) Arcane Spell Damage"] = "^%+(%d+) 奥术法术伤害",
+	["^%+(%d+) Fire Spell Damage"] = "^%+(%d+) 火焰法术伤害",
+	["^%+(%d+) Frost Spell Damage"] = "^%+(%d+) 冰霜法术伤害",
+	["^%+(%d+) Holy Spell Damage"] = "^%+(%d+) 神圣法术伤害",
+	["^%+(%d+) Nature Spell Damage"] = "^%+(%d+) 自然法术伤害",
+	["^%+(%d+) Shadow Spell Damage"] = "^%+(%d+) 暗影法术伤害",
+	["^%+(%d+) mana every 5 sec."] = "^每5秒回复(%d+)点法力值。",
+	
+	-- snowflakes ZG enchants
+	["/Hit %+(%d+)"] = "/命中 %+(%d+)",
+	["/Spell Hit %+(%d+)"] = "/法术命中 %+(%d+)",
+	["^Mana Regen %+(%d+)"] = "^法力回复 %+(%d+)",
+	["^%+(%d+) Healing Spells"] = "^%+(%d+) 治疗法术",
+	["^%+(%d+) Spell Damage and Healing"] = "^法术治疗和伤害 %+(%d+)",
+	
+	["Equip: Increases damage and healing done by magical spells and effects by up to (%d+)."] = "装备： 提高所有法术和魔法效果所造成的伤害和治疗效果，最多(%d+)点。",
+	["Equip: Increases damage and healing done by magical spells effects by up to (%d+)."] = "装备： 高所有法术和魔法效果所造成的伤害和治疗效果，最多(%d+)点。",
+	["Equip: Increases healing done by spells and effects by up to (%d+)."] = "装备： 提高法术所造成的治疗效果，最多(%d+)点。",
+	["Equip: Increases healing (%d+)."] = "装备： 使治疗法术和效果所回复的生命值提高(%d+)点。",
+	
+	-- auras
+	["Chance to hit increased by (%d)%%."] = 														"命中率提高(%d)%%。",
+	["Magical damage dealt is increased by up to (%d+)."] = 										"法术伤害提高最多(%d+)点。",
+	["Healing done by magical spells is increased by up to (%d+)."] = 								"治疗法术的效果提高最多(%d+)点。",
+	["Chance to hit reduced by (%d+)%%."] = 														"击中目标的几率降低(%d+)%%。",
+	["Chance to hit decreased by (%d+)%% and %d+ Nature damage every %d+ sec."] = 					"攻击命中率降低(%d+)%%，每%d+秒受到%d+点自然伤害。",
+	["Lowered chance to hit."] = 																	"命中率降低。", -- 5917	Fumble (25%)
+	["Increases hitpoints by 300. 15%% haste to melee attacks. 10 mana regen every 5 seconds."] = 	"生命值提高300点。近战攻击速度提高15%%。每5秒回复10点法力值。",
+	["Improves your chance to hit by (%d+)%%."] = 													"使你击中目标的几率提高(%d+)%%。",
+	["Chance for a critical hit with a spell increased by (%d+)%%."] = 								"使用法术打出致命一击的几率提高(%d+)%%。",
+	["While active, target's critical hit chance with spells and attacks increases by 10%%."] = 	"激活之后，你每一次施放火焰系伤害性法术，你的该类法术的致命一击几率都会提高10%%。",
+	["Increases attack power by %d+ and chance to hit by (%d+)%%."] = 								"攻击强度提高%d+点，击中敌人的几率提高(%d+)%%。",
+	["Holy spell critical hit chance increased by (%d+)%%."] = 										"使你的神圣法术造成致命一击的几率提高(%d+)%%。",
+	["Destruction spell critical hit chance increased by (%d+)%%."] = 								"使你的毁灭系法术的致命一击几率提高(%d+)%%。",
+	["Arcane spell critical hit chance increased by (%d+)%%.\r\nArcane spell critical hit damage increased by (%d+)%%."] = "奥术系法术造成致命一击效果的几率提高(%d+)%%。\r\n奥术系法术的致命一击伤害提高(%d+)%%。",
+	["Spell hit chance increased by (%d+)%%."] = 													"法术命中目标的几率提高(%d+)%%。",
+	
+	["Increases chance for a melee, ranged, or spell critical by (%d+)%% and all attributes by %d+."] = "打出致命一击的几率提高(%d+)%%，所有属性提高%d+点。",
+	["Melee critical-hit chance reduced by (%d+)%%.\r\nSpell critical-hit chance reduced by (%d+)%%."] = "近战武器造成致命一击的几率降低(%d+)%%。\r\n法术造成致命一击的几率降低(%d+)%%。",
+	["Increases critical chance of spells by 10%%, melee and ranged by 5%% and grants 140 attack power. 120 minute duration."] = "使法术的致命一击几率提高10%%，近战和远程攻击的致命一击几率提高5%%，攻击强度提高140点。持续120分钟。",
+	["Holy spell critical hit chance increased by (%d+)%%."] = "使你的神圣法术造成致命一击的几率提高(%d+)%%。",
+	["Destruction spell critical hit chance increased by (%d+)%%."] = "使你的毁灭系法术的致命一击几率提高(%d+)%%。",
+	["Critical strike chance with spells and melee attacks increased by (%d+)%%."] = "近战武器和法术造成致命一击的几率提高(%d+)%%。",
+	
+	
+	["MELEE_HIT_TOOLTIP"] = [[|cffffffff命中|r
+	用近战或远程武器攻击 
+	命中的结果.]],
+	
+	["SPELL_HIT_TOOLTIP"] = [[|cffffffff命中+抵抗|r
+	法术攻击 
+	命中+天赋抵抗的结果.]],
+	
+	["SPELL_HIT_SECONDARY_TOOLTIP"] = [[|cffffffff命中+抵抗 %d%% (%d%%|cff20ff20+%d%% %s|r|cffffffff)|r
+	法术攻击 
+	命中+天赋抵抗的结果.]],
+	
+	["SPELL_POWER_TOOLTIP"] = [[|cffffffff法术效果 %d|r
+	增加法术造成的伤害效果.]],
+	
+	["SPELL_POWER_SECONDARY_TOOLTIP"] = [[|cffffffff法术效果+天赋效果 %d (%d|cff20ff20+%d %s|r|cffffffff)|r
+	增加法术和天赋加成造成的伤害效果.]],
+	
+	["SPELL_HEALING_POWER_TOOLTIP"] = [[|cffffffff治疗效果+天赋效果 %d (%d|cff20ff20+%d|r|cffffffff)|r
+	增加法术和天赋加成造成的治疗效果.]],	
+	
+	["SPELL_MANA_REGEN_TOOLTIP"] = [[|cffffffff法力回复 %d (%d|cff20ff20+%d|r|cffffffff)|r
+	法力再生值每5秒.]],
+	
+	["ROGUE_MELEE_HIT_TOOLTIP"] = [[
++5% 总是击中敌对玩家.
++8% 技能总是命中团本级Boss.
++24.6% 任何攻击总是命中团本级Boss.]],
+
+	PLAYERSTAT_BASE_STATS = "主属性",
+	PLAYERSTAT_DEFENSES = "防御属性",
+	PLAYERSTAT_MELEE_COMBAT = "近战属性",
+	PLAYERSTAT_RANGED_COMBAT = "远程属性",
+	PLAYERSTAT_SPELL_COMBAT = "法术属性",
+	PLAYERSTAT_SPELL_SCHOOLS = "法术类型",
+	
+	MELEE_HIT_RATING_COLON = "命中:",
+	RANGED_HIT_RATING_COLON = "命中:",
+	SPELL_HIT_RATING_COLON = "命中:",
+	MELEE_CRIT_COLON = "暴击:",
+	RANGED_CRIT_COLON = "暴击:",
+	SPELL_CRIT_COLON = "暴击:",
+	MANA_REGEN_COLON = "法力回复:",
+	HEAL_POWER_COLON = "治疗:",
+	DODGE_COLON = DODGE .. ":",
+	PARRY_COLON = PARRY .. ":",
+	BLOCK_COLON = BLOCK .. ":",
+	
+	SPELL_POWER_COLON = "法术伤害:",
+	
+	SPELL_SCHOOL_ARCANE = "奥术",
+	SPELL_SCHOOL_FIRE = "火焰",
+	SPELL_SCHOOL_FROST = "冰霜",
+	SPELL_SCHOOL_HOLY = "神圣",
+	SPELL_SCHOOL_NATURE = "自然",
+	SPELL_SCHOOL_SHADOW = "暗影",
+	
+}
